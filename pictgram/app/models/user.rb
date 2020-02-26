617fad4573ec5d0046b578579e05d1726bca2cd2
Mizukoshi <m.mizukoshi73@gmail.com>
2020-02-26 03:19:17 +0000

    set validates

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  
+  # 半角英字半角数字それぞれ1文字以上含む8文字以上32文字以下の文字列
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i}
+  
+  # メールアドレスの正規表現を追加
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  
+  has_secure_password
 end
